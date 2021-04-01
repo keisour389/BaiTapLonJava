3a73d457cfe9de46ccbac7bb64c8ad3555168be5
keisour389 <limit389@gmail.com>
2021-04-01 17:17:09 +0700

    Handle login business

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
+  //Reference DOM by ViewChild
+  @ViewChild('emp') emp!: ElementRef;
+  @ViewChild('cus') cus!: ElementRef; 
+
+  public loginType: String = "khách hàng";
 
   constructor() { }
 
+  changeLoginType(type: String){
+    this.loginType = type;
+    //Change border after choosing login type
+    if(type === 'khách hàng'){
+      this.cus.nativeElement.style.border = "1px solid #6bd7ff";
+      this.emp.nativeElement.style.border = "none";
+    }
+    else{
+      this.emp.nativeElement.style.border = "1px solid #6bd7ff";
+      this.cus.nativeElement.style.border = "none";
+    }
+  }
+
   ngOnInit(): void {
+  }
+
+  //Setting login type is "Cus" after create template
+  ngAfterViewInit(){
+    this.cus.nativeElement.style.border = "1px solid #6bd7ff";
   }
 
 }
