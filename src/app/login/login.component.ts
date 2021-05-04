25a2c39572d07a98b06dc08e0d3a7350cadd79c7
keisour389 <limit389@gmail.com>
2021-05-04 10:55:40 +0700

    Handle validator in login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Location } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,38 +8,47 @@ import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angula
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit, AfterViewInit {
-  //Reference DOM by ViewChild
+  // Reference DOM by ViewChild
   @ViewChild('emp') emp!: ElementRef;
-  @ViewChild('cus') cus!: ElementRef; 
+  @ViewChild('cus') cus!: ElementRef;
 
-  public loginType: String = "khách hàng";
+  public loginType = 'khách hàng';
 
+  isSubmitted!: boolean;
+
+  // tslint:disable-next-line:variable-name
   constructor(private _location: Location) { }
 
-  changeLoginType(type: String){
+  changeLoginType(type: string): void{
     this.loginType = type;
-    //Change border after choosing login type
-    if(type === 'khách hàng'){
-      this.cus.nativeElement.style.border = "1px solid #0068bd";
-      this.emp.nativeElement.style.border = "none";
+    // Change border after choosing login type
+    if (type === 'khách hàng'){
+      this.cus.nativeElement.style.border = '1px solid #0068bd';
+      this.emp.nativeElement.style.border = 'none';
     }
     else{
-      this.emp.nativeElement.style.border = "1px solid #0068bd";
-      this.cus.nativeElement.style.border = "none";
+      this.emp.nativeElement.style.border = '1px solid #0068bd';
+      this.cus.nativeElement.style.border = 'none';
     }
   }
 
-  backClicked(){
-    //Return previous page
+  onSubmit(form: NgForm): void{
+    console.log(form);
+    // Check input is submitted or not
+    this.isSubmitted = true;
+  }
+
+  backClicked(): void{
+    // Return previous page
     this._location.back();
   }
 
   ngOnInit(): void {
   }
 
-  //Setting login type is "Cus" after create template
-  ngAfterViewInit(){
-    this.cus.nativeElement.style.border = "1px solid #0068bd";
+  // Setting login type is "Cus" after create template
+  ngAfterViewInit(): void{
+    this.cus.nativeElement.style.border = '1px solid #0068bd';
   }
 
 }
