fd11d96a1889095a9139e92e9ef1258cd1909891
keisour389 <limit389@gmail.com>
2021-05-13 18:13:11 +0700

    Add logout and show username in nav bar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isLogin!: boolean;
+  loginStatus!: any;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.checkIsLogin();
+  }
+
+  checkIsLogin(){
+    if(JSON.parse(localStorage.getItem('loginStatus')!) !== null){
+      this.isLogin = true;
+      this.loginStatus = JSON.parse(localStorage.getItem('loginStatus')!);
+    }
+    else{
+      this.isLogin = false;
+      console.error("Payment info is null");
+    }
+  }
+
+  logout(): void{
+    localStorage.removeItem('loginStatus');
+    document.location.reload();
   }
 
 }
